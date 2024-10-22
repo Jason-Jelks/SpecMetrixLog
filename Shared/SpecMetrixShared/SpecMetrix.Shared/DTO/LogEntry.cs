65167da6494ed0b4593b3f3f4d0aad8b3239817a
Jason Jelks <jcjelks@gmail.com>
2024-10-22 07:25:36 -0300

    Added Nuget package info added Interfaces nuget to Shared
    Successfully added Interfaces to Nuget and imported Nuget to Shared

--- Shared/SpecMetrixShared/SpecMetrix.Shared/DTO/LogEntry.cs
+++ Shared/SpecMetrixShared/SpecMetrix.Shared/DTO/LogEntry.cs
@@ -1,8 +1,27 @@
-﻿namespace SpecMetrix.Shared.DTO;
+﻿using SpecMetrix.Interfaces;
+
+namespace SpecMetrix.Shared.DTO;
 
 /// <summary>
-/// Logging
+/// Log Entry for SpecMetrix systems
 /// </summary>
-public class LogEntry
+public class LogEntry : ILogEntry
 {
+    public Guid LogId { get; set; }
+    public DateTime Timestamp { get; set; }
+    public int Code { get; set; }
+    public string Process { get; set; }
+    public LogLevel Level { get; set; }
+    public string? ClassMethod { get; set; }
+    public string Message { get; set; }
+    public ulong Occurrences { get; set; }
+    public IDictionary<string, string>? Context { get; set; }
+    public string? Source { get; set; }
+    public LogCategory? Category { get; set; }
+    public ExceptionDetails? Exception { get; set; }
+
+    public LogEntry()
+    {
+
+    }
 }
