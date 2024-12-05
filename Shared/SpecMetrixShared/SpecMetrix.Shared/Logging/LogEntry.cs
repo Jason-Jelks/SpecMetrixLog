009d95ecd016635d09b6f75a2321dd40cf178356
Jason Jelks <jcjelks@gmail.com>
2024-12-05 09:02:12 -0300

    Added MongoLogEntry
    Restored LogEntry back to last working version
    Added MongoLogEntry for MongoDB read processing

--- Shared/SpecMetrixShared/SpecMetrix.Shared/Logging/LogEntry.cs
+++ Shared/SpecMetrixShared/SpecMetrix.Shared/Logging/LogEntry.cs
@@ -1,222 +1,55 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using SpecMetrix.Interfaces;
+﻿using SpecMetrix.Interfaces;
 
-namespace SpecMetrix.Shared.Logging
+namespace SpecMetrix.Shared.Logging;
+
+/// <summary>
+/// Log Entry for SpecMetrix systems
+/// </summary>
+public class LogEntry : ILogEntry
 {
-    [BsonIgnoreExtraElements]
-    public class LogEntry : ILogEntry
+    public Guid LogId { get; set; }
+
+    public required string Namespace { get; set; }
+
+    public DateTime Timestamp { get; set; }
+
+    public required string MachineName { get; set; }
+
+    public int Code { get; set; }
+
+    public required string Process { get; set; }
+
+    public LogLevel Level { get; set; }
+
+    public string? ClassMethod { get; set; }
+
+    public required string Message { get; set; }
+
+    public ulong Occurrences { get; set; }
+
+    public IDictionary<string, string>? Metadata { get; set; }
+
+    public string? Source { get; set; }
+
+    public LogCategory? Category { get; set; }
+
+    public string? ExceptionMessage { get; set; }
+
+    public string? StackTrace { get; set; }
+
+    /// <summary>
+    /// Message Template for Serilog based logging
+    /// </summary>
+    public required string MessageTemplate { get; set; } // Holds the message template, e.g., "User {UserId} logged in from {Location} at {LoginTime}"
+    
+    public required IDictionary<string, object> TemplateValues { get; set; } // Holds the values for placeholders
+    
+    public string? DeviceName { get; set; }
+
+    public required string RenderedMessage { get; set; }
+
+    public LogEntry()
     {
-        [BsonId]
-        public ObjectId Id { get; set; }
 
-        public Guid LogId { get; set; }
-
-        public required string Message { get; set; }
-
-        [BsonElement("Properties")]
-        public required BsonDocument Properties { get; set; } = new BsonDocument(); // Default initialization
-
-        public required string MessageTemplate { get; set; }
-
-        public required IDictionary<string, object> TemplateValues { get; set; } = new Dictionary<string, object>();
-       
-        public required string RenderedMessage { get; set; }
-
-        [BsonIgnore]
-        public string Namespace
-        {
-            get
-            {
-                return Properties.Contains("Namespace") && Properties["Namespace"].IsString
-                    ? Properties["Namespace"].AsString
-                    : string.Empty;
-            }
-            set
-            {
-                EnsurePropertiesInitialized(); // Ensure Properties is not null
-                if (!string.IsNullOrEmpty(value))
-                {
-                    Properties["Namespace"] = value;
-                }
-                else
-                {
-                    Properties.Remove("Namespace");
-                }
-            }
-        }
-
-        public DateTime Timestamp { get; set; }
-
-        [BsonIgnore]
-        public string MachineName
-        {
-            get
-            {
-                return Properties.Contains("MachineName") && Properties["MachineName"].IsString
-                    ? Properties["MachineName"].AsString
-                    : string.Empty;
-            }
-            set
-            {
-                EnsurePropertiesInitialized();
-                if (!string.IsNullOrEmpty(value))
-                {
-                    Properties["MachineName"] = value;
-                }
-                else
-                {
-                    Properties.Remove("MachineName");
-                }
-            }
-        }
-
-        [BsonIgnore]
-        public int Code
-        {
-            get
-            {
-                return Properties.Contains("Code") && Properties["Code"].IsInt32
-                    ? Properties["Code"].AsInt32
-                    : default;
-            }
-            set
-            {
-                EnsurePropertiesInitialized();
-                Properties["Code"] = value;
-            }
-        }
-
-        [BsonIgnore]
-        public string Process
-        {
-            get
-            {
-                return Properties.Contains("Process") && Properties["Process"].IsString
-                    ? Properties["Process"].AsString
-                    : string.Empty;
-            }
-            set
-            {
-                EnsurePropertiesInitialized();
-                Properties["Process"] = value;
-            }
-        }
-
-        [BsonIgnore]
-        public string? ClassMethod
-        {
-            get
-            {
-                return Properties.Contains("ClassMethod") && Properties["ClassMethod"].IsString
-                    ? Properties["ClassMethod"].AsString
-                    : null;
-            }
-            set
-            {
-                EnsurePropertiesInitialized();
-                if (!string.IsNullOrEmpty(value))
-                {
-                    Properties["ClassMethod"] = value;
-                }
-                else
-                {
-                    Properties.Remove("ClassMethod");
-                }
-            }
-        }
-
-        public ulong Occurrences { get; set; }
-
-        public IDictionary<string, string>? Metadata { get; set; }
-
-        [BsonIgnore]
-        public string? Source
-        {
-            get
-            {
-                return Properties.Contains("Source") && Properties["Source"].IsString
-                    ? Properties["Source"].AsString
-                    : null;
-            }
-            set
-            {
-                EnsurePropertiesInitialized();
-                if (!string.IsNullOrEmpty(value))
-                {
-                    Properties["Source"] = value;
-                }
-                else
-                {
-                    Properties.Remove("Source");
-                }
-            }
-        }
-
-        [BsonIgnore]
-        public LogCategory? Category
-        {
-            get
-            {
-                if (Properties.Contains("Category") && Properties["Category"].IsString)
-                {
-                    return Enum.TryParse(Properties["Category"].AsString, out LogCategory category)
-                        ? category
-                        : (LogCategory?)null;
-                }
-                return null;
-            }
-            set
-            {
-                EnsurePropertiesInitialized();
-                if (value.HasValue)
-                {
-                    Properties["Category"] = value.Value.ToString();
-                }
-                else
-                {
-                    Properties.Remove("Category");
-                }
-            }
-        }
-
-        [BsonIgnore]
-        public string? ExceptionMessage { get; set; }
-
-        [BsonIgnore]
-        public string? StackTrace { get; set; }
-
-        [BsonIgnore]
-        public string? DeviceName
-        {
-            get
-            {
-                return Properties.Contains("DeviceName") && Properties["DeviceName"].IsString
-                    ? Properties["DeviceName"].AsString
-                    : null;
-            }
-            set
-            {
-                EnsurePropertiesInitialized();
-                if (!string.IsNullOrEmpty(value))
-                {
-                    Properties["DeviceName"] = value;
-                }
-                else
-                {
-                    Properties.Remove("DeviceName");
-                }
-            }
-        }
-
-        public LogLevel Level { get; set; }
-
-        private void EnsurePropertiesInitialized()
-        {
-            if (Properties == null)
-            {
-                Properties = new BsonDocument();
-            }
-        }
     }
 }
---
+++ Shared/SpecMetrixShared/SpecMetrix.Shared/Logging/MongoLogEntry.cs
@@ -0,0 +1,215 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using SpecMetrix.Interfaces;
+
+namespace SpecMetrix.Shared.Logging
+{
+    /// <summary>
+    /// Log Entry for SpecMetrix systems
+    /// </summary>
+    [BsonIgnoreExtraElements]
+    public class MongoLogEntry : ILogEntry
+    {
+        [BsonId] // Maps to MongoDB's _id field
+        public ObjectId Id { get; set; }
+
+        public Guid LogId { get; set; }
+
+        public required string Message { get; set; }
+
+        [BsonElement("Properties")]
+        public BsonDocument Properties { get; set; } = new BsonDocument();
+
+        public required string MessageTemplate { get; set; }
+
+        public required IDictionary<string, object> TemplateValues { get; set; }
+
+        public required string RenderedMessage { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public string Namespace
+        {
+            get
+            {
+                if (Properties.Contains("Namespace") && Properties["Namespace"].IsString)
+                {
+                    return Properties["Namespace"].AsString;
+                }
+                return string.Empty;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    Properties["Namespace"] = value;
+                }
+                else
+                {
+                    Properties.Remove("Namespace");
+                }
+            }
+        }
+
+        public string MachineName
+        {
+            get
+            {
+                if (Properties.Contains("MachineName") && Properties["MachineName"].IsString)
+                {
+                    return Properties["MachineName"].AsString;
+                }
+                return string.Empty;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    Properties["MachineName"] = value;
+                }
+                else
+                {
+                    Properties.Remove("MachineName");
+                }
+            }
+        }
+
+        public int Code
+        {
+            get
+            {
+                if (Properties.Contains("Code") && Properties["Code"].IsInt32)
+                {
+                    return Properties["Code"].AsInt32;
+                }
+                return default;
+            }
+            set => Properties["Code"] = value;
+        }
+
+        public string Process
+        {
+            get
+            {
+                if (Properties.Contains("Process") && Properties["Process"].IsString)
+                {
+                    return Properties["Process"].AsString;
+                }
+                return string.Empty;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    Properties["Process"] = value;
+                }
+                else
+                {
+                    Properties.Remove("Process");
+                }
+            }
+        }
+
+        public string? ClassMethod
+        {
+            get
+            {
+                if (Properties.Contains("ClassMethod") && Properties["ClassMethod"].IsString)
+                {
+                    return Properties["ClassMethod"].AsString;
+                }
+                return null;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    Properties["ClassMethod"] = value;
+                }
+                else
+                {
+                    Properties.Remove("ClassMethod");
+                }
+            }
+        }
+
+        public ulong Occurrences { get; set; }
+
+        public IDictionary<string, string>? Metadata { get; set; }
+
+        public string? Source
+        {
+            get
+            {
+                if (Properties.Contains("Source") && Properties["Source"].IsString)
+                {
+                    return Properties["Source"].AsString;
+                }
+                return null;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    Properties["Source"] = value;
+                }
+                else
+                {
+                    Properties.Remove("Source");
+                }
+            }
+        }
+
+        public LogCategory? Category
+        {
+            get
+            {
+                if (Properties.Contains("Category") && Properties["Category"].IsString)
+                {
+                    return Enum.TryParse(Properties["Category"].AsString, out LogCategory category) ? category : (LogCategory?)null;
+                }
+                return null;
+            }
+            set
+            {
+                if (value.HasValue)
+                {
+                    Properties["Category"] = value.Value.ToString();
+                }
+                else
+                {
+                    Properties.Remove("Category");
+                }
+            }
+        }
+
+        public string? ExceptionMessage { get; set; }
+
+        public string? StackTrace { get; set; }
+
+        public string? DeviceName
+        {
+            get
+            {
+                if (Properties.Contains("DeviceName") && Properties["DeviceName"].IsString)
+                {
+                    return Properties["DeviceName"].AsString;
+                }
+                return null;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    Properties["DeviceName"] = value;
+                }
+                else
+                {
+                    Properties.Remove("DeviceName");
+                }
+            }
+        }
+
+        public LogLevel Level { get; set; }
+    }
+}
